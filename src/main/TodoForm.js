5ffca73bb7357885aca25ad7b6d68cebfa28cbfc
fuad <fuad.gyulmamedov.2001@mail.ru>
2021-07-17 19:22:40 +0400

    first commit

--- src/main/TodoForm.js
+++ src/main/TodoForm.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import send_icon from './send.svg'
 
-export default function TodoForm( { todoTxt, addTodoTXT }) {
+export default function TodoForm( { addTodoTXT }) {
     let [word, setWord] = React.useState('')
 
 
     return (
         <form className='todoForm' onSubmit={(e) => addTodoTXT(e, word, setWord)}>
             <input value={word} type="text" placeholder="What needs to do done?" onChange={(e) => setWord(e.target.value)}/>
+            <button className='save__button'><img src={send_icon} width='25' height='25' alt="" /></button>
         </form>
     )
 }
